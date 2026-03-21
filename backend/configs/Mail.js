9d2cb445a8179b8d41965842d220fd7686c616c1
Raajj-Kumar-Sah <rajgupta7188@gamil.com>
2026-03-21 23:51:38 +0530

    Fix NodeMailer ETIMEDOUT by switching to Port 587 TLS

--- backend/configs/Mail.js
+++ backend/configs/Mail.js
@@ -2,9 +2,10 @@ import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 dotenv.config()
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, 
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports (like 587)
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
